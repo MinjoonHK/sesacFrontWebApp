3106e980b00a0739865f2ef02c2ddf851c3b36b7
MinjoonHK <minjoon.park.hk@gmail.com>
2024-07-07 21:10:46 +0900

    project initialize

---
+++ src/pages/login/UI/login.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@mui/material";
+import "./login.css";
+
+export const LoginPage = () => {
+  return (
+    <div>
+      <Button variant="outlined">ㅎㅇ?</Button>
+    </div>
+  );
+};
