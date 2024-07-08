ed936c464b8fa3bb155a6da1a9c4eb1199002bcd
MinjoonHK <minjoon.park.hk@gmail.com>
2024-07-08 13:36:00 +0900

    ✨ FEAT: signup, loginInput 라우팅 추가
    - 회원가입과 로그인폼 페이지로의 라우팅을 추가하였습니다

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import { LoginPage } from "../pages/login";
+import { SignUpPage } from "../pages/signUp";
+import { LoginInputPage } from "../pages/loginInput";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/logininput" element={<LoginInputPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
       </Routes>
     </>
   );
