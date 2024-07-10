bf9bff4bd8b5e6e674a3e52db3b54ef88df5a8ec
MinjoonHK <minjoon.park.hk@gmail.com>
2024-07-10 11:55:27 +0900

    🎨 STYLE: 스타일 변경
    - 인증번호, 이메일 입력란의 input 넓이 변경

--- src/pages/signUp/UI/signUp.tsx
+++ src/pages/signUp/UI/signUp.tsx
@@ -1,60 +1,56 @@
 import { TextField } from "@mui/material";
 import { useState } from "react";
 import "./signUp.css";
+import { useNavigate } from "react-router-dom";
 
 const labelSize = "1rem";
 const placeholderSize = "0.8125rem";
 
+const inputLabelProps = {
+  shrink: true,
+  style: {
+    fontSize: labelSize,
+    color: "black",
+    fontFamily: "Pretendard",
+  },
+};
+
+const sxPlaceholder = {
+  "& .MuiInputBase-input::placeholder": {
+    fontSize: placeholderSize,
+    fontFamily: "Pretendard",
+  },
+};
+
 export const SignUpPage = () => {
   const [emailSent, setEmailSent] = useState(false);
+  const navigate = useNavigate();
   return (
     <div className="signup-page-wrapper">
       <div className="signup-page-inner-wrapper">
         <div className="input-group">
           <TextField
+            required
+            style={{ width: "236px" }}
             label="아이디(이메일)"
             type="email"
-            InputLabelProps={{
-              shrink: true,
-              style: {
-                fontSize: labelSize,
-                color: "black",
-                fontFamily: "Pretendard",
-                fontWeight: "bold",
-              },
-            }}
+            InputLabelProps={inputLabelProps}
             placeholder="이메일 주소"
             variant="standard"
-            sx={{
-              "& .MuiInputBase-input::placeholder": {
-                fontSize: placeholderSize,
-                fontFamily: "Pretendard",
-              },
-            }}
+            sx={sxPlaceholder}
           />
           <button className="check-button">중복확인</button>
         </div>
         <div className="input-group">
           <TextField
+            required
+            style={{ width: "236px" }}
             label="인증번호"
             type="email"
-            InputLabelProps={{
-              shrink: true,
-              style: {
-                fontSize: labelSize,
-                color: "black",
-                fontFamily: "Pretendard",
-                fontWeight: "bold",
-              },
-            }}
-            placeholder="인증번호 입력"
+            InputLabelProps={inputLabelProps}
+            placeholder="이메일로 전송된 인증번호를 입력해주세요"
             variant="standard"
-            sx={{
-              "& .MuiInputBase-input::placeholder": {
-                fontSize: placeholderSize,
-                fontFamily: "Pretendard",
-              },
-            }}
+            sx={sxPlaceholder}
           />
           <button
             className="send-button"
@@ -68,109 +64,58 @@ export const SignUpPage = () => {
         </div>
 
         <TextField
+          required
           label="비밀번호"
           type="password"
-          InputLabelProps={{
-            shrink: true,
-            style: {
-              fontSize: labelSize,
-              color: "black",
-              fontFamily: "Pretendard",
-              fontWeight: "bold",
-            },
-          }}
+          InputLabelProps={inputLabelProps}
           placeholder="비밀번호"
           variant="standard"
-          sx={{
-            "& .MuiInputBase-input::placeholder": {
-              fontSize: placeholderSize,
-              fontFamily: "Pretendard",
-            },
-          }}
+          sx={sxPlaceholder}
         />
         <TextField
+          required
           label="비밀번호 확인"
-          InputLabelProps={{
-            shrink: true,
-            style: {
-              fontSize: labelSize,
-              color: "black",
-              fontFamily: "Pretendard",
-              fontWeight: "bold",
-            },
-          }}
+          type="password"
+          InputLabelProps={inputLabelProps}
           placeholder="비밀번호 확인"
           variant="standard"
-          sx={{
-            "& .MuiInputBase-input::placeholder": {
-              fontSize: placeholderSize,
-              fontFamily: "Pretendard",
-            },
-          }}
+          sx={sxPlaceholder}
         />
         <TextField
+          required
           label="이름"
-          InputLabelProps={{
-            shrink: true,
-            style: {
-              fontSize: labelSize,
-              color: "black",
-              fontFamily: "Pretendard",
-              fontWeight: "bold",
-            },
-          }}
+          InputLabelProps={inputLabelProps}
           placeholder="실명을 입력해주세요"
           variant="standard"
-          sx={{
-            "& .MuiInputBase-input::placeholder": {
-              fontSize: placeholderSize,
-              fontFamily: "Pretendard",
-            },
-          }}
+          sx={sxPlaceholder}
         />
         <TextField
+          required
           label="주소"
-          InputLabelProps={{
-            shrink: true,
-            style: {
-              fontSize: labelSize,
-              color: "black",
-              fontFamily: "Pretendard",
-              fontWeight: "bold",
-            },
-          }}
+          InputLabelProps={inputLabelProps}
           placeholder="거주중인 주소를 입력해주세요"
           variant="standard"
-          sx={{
-            "& .MuiInputBase-input::placeholder": {
-              fontSize: placeholderSize,
-              fontFamily: "Pretendard",
-            },
-          }}
+          sx={sxPlaceholder}
         />
         <TextField
+          required
           label="생년월일"
-          InputLabelProps={{
-            shrink: true,
-            style: {
-              fontSize: labelSize,
-              color: "black",
-              fontFamily: "Pretendard",
-              fontWeight: "bold",
-            },
-          }}
+          InputLabelProps={inputLabelProps}
           placeholder="생년월일을 입력해주세요"
           variant="standard"
-          sx={{
-            "& .MuiInputBase-input::placeholder": {
-              fontSize: placeholderSize,
-              fontFamily: "Pretendard",
-            },
-          }}
+          sx={sxPlaceholder}
         />
       </div>
       <div className="signup-form-submit-button-wrapper">
-        <button className="submit-button">조금이 시작하기</button>
+        <button
+          onClick={() => {
+            navigate("/login");
+          }}
+          type="submit"
+          className="submit-button"
+        >
+          조금이 시작하기
+        </button>
       </div>
     </div>
   );
