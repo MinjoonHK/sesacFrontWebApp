9c58d85a3019862644b270e53656ba308caa3675
MinjoonHK <minjoon.park.hk@gmail.com>
2024-07-24 12:08:50 +0900

    🎨 STYLE: SCDream6 폰트추가
    - SCDream6 폰트 추가

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import { LoginPage } from "../pages/login";
 import { SignUpPage } from "../pages/signUp";
 import { LoginInputPage } from "../pages/loginInput";
+import { ChatUI } from "../pages/chatbot";
+import "./App.css";
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
         <Route path="/" element={<LoginPage />} />
         <Route path="/login" element={<LoginInputPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/chat" element={<ChatUI />} />
       </Routes>
     </>
   );
