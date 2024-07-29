f1927f3855a331e93665d9a05321625f8f15f667
MinjoonHK <minjoon.park.hk@gmail.com>
2024-07-30 00:34:42 +0900

    🎨 STYLE: 불필요한 이미지파일 삭제 및 theme 제거
    - 불필요한 이미지 파일 삭제와 theme을 제거하였습니다
    - 192x192, 512x512 이미지 파일을 추가하였습니다

--- vite.config.ts
+++ vite.config.ts
@@ -16,44 +16,13 @@ export default defineConfig({
         theme_color: "#000000",
         icons: [
           {
-            src: "/img/apple-touch-icon-57x57.png",
-            sizes: "57x57",
+            src: "/img/apple-touch-icon-192x192.png",
+            sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/img/apple-touch-icon-60x60.png",
-            sizes: "60x60",
-            type: "image/png",
-          },
-          {
-            src: "/img/apple-touch-icon-72x72.png",
-            sizes: "72x72",
-            type: "image/png",
-          },
-          {
-            src: "/img/apple-touch-icon-76x76.png",
-            sizes: "76x76",
-            type: "image/png",
-          },
-          {
-            src: "/img/apple-touch-icon-114x114.png",
-            sizes: "114x114",
-            type: "image/png",
-          },
-          {
-            src: "/img/apple-touch-icon-120x120.png",
-            sizes: "120x120",
-            type: "image/png",
-          },
-
-          {
-            src: "/img/apple-touch-icon-144x144.png",
-            sizes: "144x144",
-            type: "image/png",
-          },
-          {
-            src: "/img/apple-touch-icon-152x152.png",
-            sizes: "152x152",
+            src: "/img/apple-touch-icon-512x512.png",
+            sizes: "512x512",
             type: "image/png",
           },
         ],
