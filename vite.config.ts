bd3f0674dc1ff4ab47502e0b3fbe733a1487a99e
MinjoonHK <minjoon.park.hk@gmail.com>
2024-07-30 00:20:21 +0900

    ✨ FEAT: 앱 설치 기능 추가와 icon을 위한 이미지 파일 추가와
    - pwa 를 활용하여 모바일 및 데스크톱에서 설치할수있도록 변경하였습니다

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,66 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server:{
-    port:3000,
-  }
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "가출청소년 자립지원 AI 당찬이",
+        short_name: "당찬이",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "/img/apple-touch-icon-57x57.png",
+            sizes: "57x57",
+            type: "image/png",
+          },
+          {
+            src: "/img/apple-touch-icon-60x60.png",
+            sizes: "60x60",
+            type: "image/png",
+          },
+          {
+            src: "/img/apple-touch-icon-72x72.png",
+            sizes: "72x72",
+            type: "image/png",
+          },
+          {
+            src: "/img/apple-touch-icon-76x76.png",
+            sizes: "76x76",
+            type: "image/png",
+          },
+          {
+            src: "/img/apple-touch-icon-114x114.png",
+            sizes: "114x114",
+            type: "image/png",
+          },
+          {
+            src: "/img/apple-touch-icon-120x120.png",
+            sizes: "120x120",
+            type: "image/png",
+          },
+
+          {
+            src: "/img/apple-touch-icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+          {
+            src: "/img/apple-touch-icon-152x152.png",
+            sizes: "152x152",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+  server: {
+    port: 3000,
+  },
+});
