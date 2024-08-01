0d741a47d9d5c44dfe6f3e76b05b55733a45ce40
MinjoonHK <minjoon.park.hk@gmail.com>
2024-08-01 17:41:19 +0900

    ✨ FEAT: 챗봇 기능 추가

--- src/pages/chatbot/UI/chatbot.tsx
+++ src/pages/chatbot/UI/chatbot.tsx
@@ -1,8 +1,51 @@
 import { useNavigate } from "react-router-dom";
 import "./chatbot.css";
+import { useState } from "react";
+import axios from "axios";
+import { TypeAnimation } from "react-type-animation";
 
 export const ChatUI = () => {
   const navigate = useNavigate();
+  const [message, setMessage] = useState("");
+  const [chatMessages, setChatMessages] = useState([
+    {
+      sender: "bot",
+      text: "반가워요! 어떤정보를 찾고 계신가요?",
+    },
+  ]);
+
+  const handleKeyUp = async (event: any) => {
+    if (event.key === "Enter" && message.trim()) {
+      const newMessage = { sender: "user", text: message };
+      setChatMessages((prevMessages) => [...prevMessages, newMessage]);
+      setMessage("");
+
+      try {
+        const res = await axios.post("http://203.234.62.82/api/question/chat", {
+          content: JSON.stringify({ message }),
+        });
+        const botMessage = {
+          sender: "bot",
+          text:
+            res.data.answer.response ||
+            "오류가 발생했습니다. 다시 시도해주세요.",
+        };
+        setChatMessages((prevMessages) => [...prevMessages, botMessage]);
+      } catch (err) {
+        console.log(err);
+        const errorMessage = {
+          sender: "bot",
+          text: "서버와의 통신 중 오류가 발생했습니다.",
+        };
+        setChatMessages((prevMessages) => [...prevMessages, errorMessage]);
+      }
+    }
+  };
+
+  const handleOnChange = (event: any) => {
+    setMessage(event.target.value);
+  };
+
   return (
     <div>
       <div>
@@ -41,6 +84,68 @@ export const ChatUI = () => {
         </div>
         <div
           style={{
+            marginTop: "7.346vh",
+            padding: "0 29px",
+            width: "100%",
+            height: "55vh",
+            overflowY: "scroll",
+            borderTop: "1px solid #DEDEDE",
+            borderBottom: "1px solid #DEDEDE",
+          }}
+        >
+          {chatMessages.map((chat, index) => (
+            <div
+              key={index}
+              style={{
+                display: "flex",
+
+                justifyContent:
+                  chat.sender === "user" ? "flex-end" : "flex-start",
+              }}
+            >
+              {chat.sender === "bot" && (
+                <div>
+                  <img
+                    src="/img/chat_dangchan_icon.svg"
+                    alt="이미지를 표시할 수 없습니다!"
+                  />
+                </div>
+              )}
+              <div
+                style={{
+                  marginLeft: "10px",
+                  display: "flex",
+                  flexDirection: "column",
+                  fontFamily: "Pretendard-medium",
+                  fontSize: "13px",
+                  marginBottom: "15px",
+                }}
+              >
+                {chat.sender === "bot" && (
+                  <div style={{ marginBottom: "8px" }}>당찬이</div>
+                )}
+                <div
+                  style={{
+                    color: chat.sender === "bot" ? "#B1B5B8" : "white",
+                    backgroundColor:
+                      chat.sender === "bot" ? "#EEF2F5" : "#3561FF",
+                    borderRadius: "10px",
+                    padding: "13px",
+                  }}
+                >
+                  <TypeAnimation
+                    sequence={[chat.text]}
+                    speed={50}
+                    cursor={false}
+                  />
+                  {/* {chat.text} */}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div
+          style={{
             bottom: "5.569vh",
             position: "fixed",
             width: "100%",
@@ -50,6 +155,9 @@ export const ChatUI = () => {
           }}
         >
           <input
+            onKeyUp={handleKeyUp}
+            onChange={handleOnChange}
+            value={message}
             placeholder="메시지를 입력해주세요"
             style={{
               backgroundColor: "#EEF2F5",
@@ -58,6 +166,7 @@ export const ChatUI = () => {
               paddingTop: "13px",
               paddingLeft: "21px",
               height: "5.569vh",
+              width: "100%",
             }}
           ></input>
         </div>
