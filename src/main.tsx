0da95e06c0aeb054404eb6c6df38ad7afec44615
MinjoonHK <minjoon.park.hk@gmail.com>
2024-11-26 00:33:43 +0900

    ♻️ REFACTOR: Router 방식 변경에 따라 main.tsx에서 BrowserRouter 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import ReactDOM from "react-dom/client";
 import App from "./app/App.tsx";
-import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
 
 axios.defaults.baseURL = import.meta.env.VITE_SERVER_URL;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <App />  
 );
